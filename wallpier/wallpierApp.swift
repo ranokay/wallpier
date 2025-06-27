3178569ef3fd46710ce467a31bd3329ae2f84492
ranokay <github@ranokay.com>
2025-06-28 01:48:48 +0300

    Initial Commit

---
+++ wallpier/wallpierApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallpierApp.swift
+//  wallpier
+//
+//  Created by Yuuta on 28.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct wallpierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wallpierTests/wallpierTests.swift
@@ -0,0 +1,17 @@
+//
+//  wallpierTests.swift
+//  wallpierTests
+//
+//  Created by Yuuta on 28.06.2025.
+//
+
+import Testing
+@testable import wallpier
+
+struct wallpierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
